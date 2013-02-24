64a66c20da7265e78c017da6f55b25b0ad16fbe6
dzucconi <damonzucconi@gmail.com>
2013-02-24 16:03:45 -0500

    Switches to zepto; fallback for tap; clean up

--- js/app.js
+++ js/app.js
@@ -1,44 +1,52 @@
-$(function() {
+(function() {
+  "use strict";
+
+  var Utils = {
+    events: { click : "click" }
+  }
+
+  if (typeof window.ontouchstart !== "undefined") {
+    Utils.events.click = "tap";
+  }
+
   soundManager.debugMode     = false;
   soundManager.preferFlash   = false; 
   soundManager.useHTML5Audio = true;
-  
   soundManager.onready(function() {
-    var pop,
-        $moon,
-        hideMoon,
-        showMoon,
-        init;
+    var Daymoon = {
+      $el: $("#moon"),
 
-    pop = soundManager.createSound({
-      id: "pop",
-      url: "audio/pop.mp3",
-      autoLoad: true,
-      autoPlay: false,
-      loops: 1,
-      volume: 100
+      pop: soundManager.createSound({
+        id       : "pop",
+        url      : "audio/pop.mp3",
+        autoLoad : true,
+        autoPlay : false,
+        loops    : 1,
+        volume   : 100
+      }),
+
+      hide: function() {
+        // Fade out with a duration of 84ms
+        Daymoon.$el.animate({ opacity: 0 }, 84);
+      },
+
+      show: function() {
+        Daymoon.$el.css({ opacity: 1 });
+        
+        Daymoon.hide();
+      },
+
+      strobe: function() {
+        // Plays immediately
+        Daymoon.pop.play();
+
+        // Displays moon after 56ms
+        setTimeout(Daymoon.show, 56);
+      }
+    }
+
+    $("body").on(Utils.events.click, function(e) {
+      Daymoon.strobe();
     });
-
-    $moon = $("#moon");
-
-    hideMoon = function() {
-      $moon.fadeOut(84); // Fades out in 84ms
-    }
-
-    showMoon = function() {
-      $moon.show();
-      hideMoon();
-    }
-
-    init = function() {
-      pop.play(); // Plays immediately
-      setTimeout(showMoon, 56); // Displays moon after 56ms
-    }
-
-    // setInterval(init, 2000);
-    
-    $("body").on("click", function() {
-      init();
-    });
-  });
-});
+  }); // soundManager.onready
+})();
